af0c140ab2d5c29b9d5664b3116d9aa97d6a83fd
oopsss1 <142162317+oopsss1@users.noreply.github.com>
2024-04-12 16:58:08 +0800

    Update main.js

--- main.js
+++ main.js
@@ -20,3 +20,30 @@ export function createApp() {
   }
 }
 // #endif
+
+
+// 下面的这些是封装的
+// import App from './App'
+// // #ifndef VUE3
+// import Vue from 'vue'
+// Vue.config.productionTip = false
+
+// App.mpType = 'app'
+// const app = new Vue({
+//   ...App
+// })
+// app.$mount('#app')
+// // #endif
+
+// // #ifdef VUE3
+// import { createSSRApp } from 'vue'
+// import request from './utils/request.js'
+// export function createApp() {
+//   const app = createSSRApp(App)
+//   // app.config.globalProperties.$request = request
+//   app.config.globalProperties.$axios = request
+//   return {
+//     app
+//   }
+// }
+// // #endif
